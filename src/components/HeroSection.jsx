939411cf1913d3b1c1b0b6a6fcd9fa76a515c2c4
Ridho Esa <ridhoesaprayoga22@gmail.com>
2024-12-23 17:07:50 +0700

    improved feature and color

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import bg from '../assets/bg.jpeg';
 
 const HeroSection = () => {
   return (
-    <div className="relative mb-8 bg-gradient-to-r from-green-200 to-blue-200 rounded-xl p-8">
+    <div className="relative mb-8 px-10" style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       <div className="flex justify-between items-center">
         <div>
-          <h2 className="text-3xl font-bold mb-2">Rumah Makan Enigmanians</h2>
-          <p className="text-gray-600 mb-4">Jl. Topaz, No. 7, Kota Malang</p>
-          <p className="text-xl mb-4">Buka 24 Jam</p>
+          <h2 className="text-white text-3xl font-bold mb-2">Rumah Makan Enigmanians</h2>
+          <p className="text-white mb-4 font-semibold">Jl. Topaz, No. 7, Kota Malang</p>
+          <p className="text-xl mb-4 text-white">Buka 24 Jam</p>
           <button className="bg-blue-500 text-white px-6 py-2 rounded-lg">
             Beli Sekarang
           </button>
